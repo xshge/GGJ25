67eaa5d68ea1ec598d954e29b0158e1207e2996e
toastyghostii <kikithecoolcookie@gmail.com>
2025-01-25 01:40:39 -0500

    edited bubble shooter!

--- Assets/Scripts/Bubble_Shooter.cs
+++ Assets/Scripts/Bubble_Shooter.cs
@@ -6,6 +6,7 @@ public class Bubble_Shooter : MonoBehaviour
 {
     //for testing purposes, whether we're having the bubble shoot straight up or based on where the player aims
     public bool BubbleAimable;
+
     // instantiation parameters
     public GameObject bubblePrefab;
     public Transform spawnerTransform;
@@ -18,11 +19,9 @@ public class Bubble_Shooter : MonoBehaviour
 
     //bubble launch force
     public float launchForce;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    //checking how long the button has been held
+    public float timer;
 
     // Update is called once per frame
     void Update()
@@ -32,16 +31,31 @@ public class Bubble_Shooter : MonoBehaviour
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         // Xinyi can you do the input system stuff for me please and thank you
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonUp(0))
         {
             LaunchBubble();
+        }
+
+        //checks if the left mouse button is being held down
+        if (Input.GetMouseButton(0))
+        {
+            timer += Time.deltaTime;
         }
     }
 
     void LaunchBubble()
     {
+        //maxes out the time a bubble can be held for at 3 seconds
+        if (timer > 3)
+        {
+            timer = 3;
+        }
+
         //calculates the direction the bubble should travel in
         bubbleDirection = mousePos - playerPos;
+
+        //if we end up chosing to aim the bubble, i'm gonna max out the x and y values of the bubbleDirection vector at like 2 & scale it so they're at the same speed rather than speed being impacted by how far away from the player the mouse is clicked
+        Debug.Log(bubbleDirection);
 
         //summons the bubble
         GameObject newBubble = Instantiate(bubblePrefab, spawnerTransform.position, Quaternion.identity);
@@ -49,11 +63,14 @@ public class Bubble_Shooter : MonoBehaviour
         //launches the bubble
         if (BubbleAimable)
         {
-            newBubble.GetComponent<Rigidbody2D>().AddForce(bubbleDirection * .5f, ForceMode2D.Impulse);
+            newBubble.GetComponent<Rigidbody2D>().AddForce(bubbleDirection * .5f * timer, ForceMode2D.Impulse);
         }
         else // if the bubble needs to shoot straight up
         {
-            newBubble.GetComponent<Rigidbody2D>().AddForce( new Vector2(0,launchForce), ForceMode2D.Impulse);
+            newBubble.GetComponent<Rigidbody2D>().AddForce( new Vector2(0,launchForce * timer), ForceMode2D.Impulse);
         }
+
+        //resets timer
+        timer = 0;
     }
 }
