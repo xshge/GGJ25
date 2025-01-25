d8764746899cc61d4f8004154f81259e39aa9487
xshenge <xshen12138@outlook.com>
2025-01-25 01:52:13 -0500

    Old Input System

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -17,15 +17,31 @@ public class CharacterController : MonoBehaviour
     {
         _pRB = GetComponent<Rigidbody2D>();
     }
-    void FixedUpdate()
+    void Update()
     {
         if (_pRB != null)
         {
-            _pRB.AddForce(MoveVector * _sideMoveForce);
+            MoveVector = new Vector2(Input.GetAxis("Horizontal"), 0).normalized;
+           
         }
        
     }
-    public void MovementInput(InputAction.CallbackContext context)
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D) ||Input.GetKey(KeyCode.A) )
+            {
+            // Debug.Log(MoveVector);
+            _pRB.velocity = MoveVector * _sideMoveForce;
+            }
+        if(Input.GetKeyDown(KeyCode.Space)) _pRB.AddForce(new Vector2(0, 1) * _upwardForce, ForceMode2D.Impulse);
+    }
+    Vector2 calcMove()
+    {
+        Vector2 moveVector = Vector2.left * Input.GetAxis("Horizontal");
+        Debug.Log(moveVector);
+        return moveVector;
+    }
+    private void MovementInput(InputAction.CallbackContext context)
     {
         MoveVector = context.ReadValue<Vector2>();
     }
