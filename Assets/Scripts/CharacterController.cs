a692d8a9c3dc65dfe68097de9372d711444b2c02
toastyghostii <kikithecoolcookie@gmail.com>
2025-02-23 12:24:15 -0500

    3D movement

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -5,35 +5,37 @@ using UnityEngine.InputSystem;
 
 public class CharacterController : MonoBehaviour
 {
-    Vector2 MoveVector;
-    Rigidbody2D _pRB;
+    Vector3 MoveVector;
+    Rigidbody _pRB;
     float timer = 0f;
     [SerializeField] private float _sideMoveForce;
     [SerializeField] private float _upwardForce;
     public bool isunderWater = false;
+
+    public GameObject realDaisy;
     Animator _animate;
     public SpriteRenderer daisy;
     float levelChange = 1;
 
     void Start()
     {
-        _pRB = GetComponent<Rigidbody2D>();
-        _animate = GetComponent<Animator>();
+        _pRB = GetComponent<Rigidbody>();
+        _animate = realDaisy.GetComponent<Animator>();
     }
     void Update()
-    {   
+    {
         timer -= Time.deltaTime;
-        
+
         if (_pRB != null)
-        {   
+        {
             //calculate vector and updating it.
-            MoveVector = new Vector2(Input.GetAxis("Horizontal"), 0).normalized;
-           
+            MoveVector = new Vector3(Input.GetAxis("Horizontal"), 0).normalized;
+
         }
         if (Input.GetKeyDown(KeyCode.W) && timer <= 0f)
-        {   
+        {
             timer = 0.75f;
-            _pRB.AddForce(new Vector2(0, 1) * _upwardForce, ForceMode2D.Impulse);
+            _pRB.AddForce(new Vector3(0, 1) * _upwardForce, ForceMode.Impulse);
             _animate.SetBool("up", true);
         }
         else
@@ -44,18 +46,18 @@ public class CharacterController : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.D) ||Input.GetKey(KeyCode.A) )
-            {
-                // Debug.Log(MoveVector); all the actuall physic movement
-                if(_pRB.bodyType == RigidbodyType2D.Dynamic)
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+        {
+            // Debug.Log(MoveVector); all the actuall physic movement
+            if (_pRB.useGravity)
             {
                 if (isunderWater)
                 {
                     // _pRB.velocity = MoveVector * levelChange * (_sideMoveForce/2);
-                    _pRB.AddForce(MoveVector * levelChange * (_sideMoveForce), ForceMode2D.Force);
-                    _pRB.AddForce(Vector2.up * 5f);
+                    _pRB.AddForce(MoveVector * levelChange * (_sideMoveForce), ForceMode.Force);
+                    _pRB.AddForce(Vector3.up * 5f);
                 }
-               
+
             }
             _animate.SetBool("moving", true);
         }
@@ -63,7 +65,7 @@ public class CharacterController : MonoBehaviour
         {
             _animate.SetBool("moving", false);
         }
-        
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -84,7 +86,8 @@ public class CharacterController : MonoBehaviour
     {
         //play animation;
         _animate.SetBool("Hit", true);
-        _pRB.bodyType = RigidbodyType2D.Static;
+        _pRB.useGravity = false;
+        _pRB.isKinematic = true;
         yield return new WaitForSeconds(1f);
         daisy.enabled = false;
         yield return new WaitForSeconds(1.5f);
@@ -94,13 +97,13 @@ public class CharacterController : MonoBehaviour
     #region Redacted InputSystem code
     private void MovementInput(InputAction.CallbackContext context)
     {
-        MoveVector = context.ReadValue<Vector2>();
+        MoveVector = context.ReadValue<Vector3>();
     }
     public void FloatUp(InputAction.CallbackContext context)
     {
         if (context.started)
         {
-            _pRB.AddForce(new Vector2(0, 1) * _upwardForce, ForceMode2D.Impulse);
+            _pRB.AddForce(new Vector3(0, 1) * _upwardForce, ForceMode.Impulse);
         }
     }
     #endregion
