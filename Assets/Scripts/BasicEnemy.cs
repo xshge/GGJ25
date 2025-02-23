078c11aa13df9ac7b8870cfca2c9598dfbdcbd01
xshenge <xshen12138@outlook.com>
2025-02-23 17:55:27 -0500

    Basic Set Up for default Enemy

--- Assets/Scripts/BasicEnemy.cs
+++ Assets/Scripts/BasicEnemy.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class BasicEnemy : MonoBehaviour
 {
@@ -10,11 +11,14 @@ public class BasicEnemy : MonoBehaviour
     public float Speed = 1f;
     public float RotAngleZ = 45;
     public LayerMask _castLayer;
-    public bool isActive;
+    public bool isAlive;
+
+    [SerializeField] private Transform _gunpoint;
     
     private Transform Target;
     private Coroutine LookCoroutine;
     private Coroutine Sweeping;
+  
 
     void Start()
     {
@@ -23,8 +27,10 @@ public class BasicEnemy : MonoBehaviour
     }
     private IEnumerator sweepDetect()
     {
+        bool _detectedPlayer = false;
+
         //detecting the player;
-        while (true)
+        while (!_detectedPlayer)
         {
             float rY = Mathf.SmoothStep(0, RotAngleZ, Mathf.PingPong(Time.time * Speed, 1));
             transform.rotation = Quaternion.Euler(0, 0, rY);
@@ -34,26 +40,68 @@ public class BasicEnemy : MonoBehaviour
             if (result)
             {
                 Debug.Log(result.collider);
+                _detectedPlayer = true;
+                isAlive= true;
+                StartRotating(result.transform);
             }
             yield return null;
         }
         
     }
 
-    public void StartRotating()
+    public void StartRotating(Transform player)
     {
         if (LookCoroutine != null)
         {
             StopCoroutine(LookCoroutine);
         }
 
-        LookCoroutine = StartCoroutine(LookAt());
+        LookCoroutine = StartCoroutine(LookAt(player));
     }
 
-    private IEnumerator LookAt()
+    private IEnumerator LookAt(Transform plyr)
     {
-        Debug.Log("lookinh");
-        yield return null;
+        //Debug.Log("lookinh");
+        float remainingDistance, dist;
+        float bulletSpeed = 3;
+        float time = 0;
+
+        Vector3 direction = plyr.position - transform.position;
+        Quaternion initialRotation = transform.rotation;
+        Quaternion lookRotation = Quaternion.LookRotation( transform.forward, plyr.position - transform.position);
+       
+        while (time < 1)
+        {
+           transform.rotation = Quaternion.Slerp(initialRotation, lookRotation, time);
+
+            time += Time.deltaTime * 5;
+
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(0.5f);
+        //instantiate bullets;
+        GameObject _blt = Instantiate(_projectile, _gunpoint.position, Quaternion.identity);
+        //debug ray;
+        Vector3 StartPos = _blt.transform.position;
+        remainingDistance = Vector3.Distance(StartPos, plyr.position);
+        dist = remainingDistance;
+
+        //bullet traveling;
+        while (remainingDistance > 0f)
+        {
+            _blt.transform.position = Vector3.Lerp(StartPos, plyr.position, 1 - (remainingDistance / dist));
+            remainingDistance -= bulletSpeed * Time.deltaTime;
+            yield return null;
+        }
+
+        if (isAlive == true)
+        {
+            StartCoroutine(LookAt(plyr));
+        }
+
+
+       
     }
 
     void OnDrawGizmos()
